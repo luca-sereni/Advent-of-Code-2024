0b6bc558107af87452ae51f36e7499965eba72d4
luca_sereni <luca1.sereni@mail.polimi.it>
2025-04-16 15:31:10 +0200

    updated first part to support the second part

--- day10/day10.py
+++ day10/day10.py
@@ -1,26 +1,30 @@
-def walk(map: list, i: int, j: int, last_position_value: int, visited: list) -> int:
+def walk(map: list, i: int, j: int, last_position_value: int, visited: list, is_first_part: bool) -> int:
     if map[i][j] != last_position_value + 1:
         return 0
-    if map[i][j] == 9 and (i, j) not in visited:
-        visited.append((i, j))
-        return 1
+    if is_first_part:
+        if map[i][j] == 9 and (i, j) not in visited:
+            visited.append((i, j))
+            return 1
+    else:
+        if map[i][j] == 9:
+            return 1
     count = 0
 
     # Go left
     if j > 0:
-        count += walk(map, i, j - 1, map[i][j], visited)
+        count += walk(map, i, j - 1, map[i][j], visited, is_first_part)
 
     # Go right
     if j < len(map[i]) - 1:
-        count += walk(map, i, j + 1, map[i][j], visited)
+        count += walk(map, i, j + 1, map[i][j], visited, is_first_part)
 
     # Go up
     if i > 0:
-        count += walk(map, i - 1, j, map[i][j], visited)
+        count += walk(map, i - 1, j, map[i][j], visited, is_first_part)
 
     # Go down
     if i < len(map) - 1:
-        count += walk(map, i + 1, j, map[i][j], visited)
+        count += walk(map, i + 1, j, map[i][j], visited, is_first_part)
     
     return count
 
@@ -38,10 +42,19 @@ for i, line in enumerate(lines):
         if n == 0:
             trailheads.append((i, j))
 
+# First part
 sum = 0
 for trailhead in trailheads:
     visited = []
-    count = walk(map, trailhead[0], trailhead[1], -1, visited)
+    count = walk(map, trailhead[0], trailhead[1], -1, visited, True)
+    sum += count
+
+print(sum)
+
+# Second part
+sum = 0
+for trailhead in trailheads:
+    count = walk(map, trailhead[0], trailhead[1], -1, [], False)
     sum += count
 
 print(sum)
