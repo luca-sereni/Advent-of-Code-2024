7c8583f963f5a04c36c58f851370684696d525bc
luca_sereni <luca1.sereni@mail.polimi.it>
2025-05-08 10:23:10 +0200

    added day18 part 2

--- day18/sol.py
+++ day18/sol.py
@@ -1,16 +1,28 @@
+from collections import deque
+
 possible_directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-def bfs(start, grid, end) -> int:
-    queue = [(start, 0)]
+def bfs(start, grid, end) -> list:
+    # BFS algorithm to find the shortest path in a grid
+    minimum_path = []
+    queue = deque([(start, 0)])
     visited = set()
     visited.add(start)
+    parent = {}
 
     while queue:
-        current = queue.pop(0)
-        curr_pos = current[0]
-        curr_score = current[1]
+        curr_pos, curr_score = queue.popleft()
         if curr_pos == end:
-            return curr_score
+            path = []
+            while curr_pos != start:
+                path.append(curr_pos)
+                curr_pos = parent[curr_pos]
+            path.append(start)
+            path.reverse()
+            if not minimum_path or len(path) < len(minimum_path):
+                minimum_path = path
+
+            return minimum_path
 
         for direction in possible_directions:
             next_x = curr_pos[0] + direction[0]
@@ -20,7 +32,11 @@ def bfs(start, grid, end) -> int:
                 next_pos = (next_x, next_y)
                 if next_pos not in visited:
                     visited.add(next_pos)
+                    parent[next_pos] = curr_pos
                     queue.append((next_pos, curr_score + 1))
+
+    return minimum_path
+                    
 
 def print_grid(grid):
     for row in grid:
@@ -49,5 +65,19 @@ for i in range(1024):
 
     grid[y][x] = '#'
 
-print("Score: {}".format(bfs((0, 0), grid, (70, 70))))
-#print_grid(grid)
+minimum_path = bfs((0, 0), grid, (70, 70))
+print("Score: {}".format(len(minimum_path) - 1))
+
+# Part 2
+# We add one obstacle at a time and check if a path can be found
+# If not, we print the coordinates of this last obstacle added.
+
+for i in range(1025, len(lines)):
+    x = lines[i][0]
+    y = lines[i][1]
+
+    grid[y][x] = '#'
+    minimum_path = bfs((0, 0), grid, (70, 70))
+    if len(minimum_path) == 0:
+        print(x, y)
+        break
