ccce37ff780f54b627596f75d46d2503271da7bc
luca_sereni <luca1.sereni@mail.polimi.it>
2025-05-20 19:58:58 +0200

    added day24 part 2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -88,6 +88,33 @@ impl Wire {
     }
 }
 
+#[derive(Debug)]
+pub struct Adder {
+    input_x: String,
+    input_y: String,
+    input_carry: String,
+    and_input_signals: String,
+    xor_input_signals: String,
+    and_before_carry: String,
+    output: String,
+    output_carry: String,
+}
+
+impl Adder {
+    fn new(input_x: &str, input_y: &str, input_carry: &str, and_input_signals: &str, xor_input_signals: &str, and_before_carry: &str, output: &str, output_carry: &str) -> Self {
+        Adder {
+            input_x: input_x.to_string(),
+            input_y: input_y.to_string(),
+            input_carry: input_carry.to_string(),
+            and_input_signals: and_input_signals.to_string(),
+            xor_input_signals: xor_input_signals.to_string(),
+            and_before_carry: and_before_carry.to_string(),
+            output: output.to_string(),
+            output_carry: output_carry.to_string(),
+        }
+    }
+}
+
 fn parse_file(input: &str, wires: &mut HashSet<Wire>, operations: &mut Vec<Operation>) {
     let (part1, part2) = input.split_once("\n\n").unwrap();
 
@@ -148,6 +175,210 @@ fn part1(wires: &HashSet<Wire>) -> usize {
     final_value
 }
 
+fn part2(wires: &HashSet<Wire>, operations: &Vec<Operation>) {
+    let mut x_values = wires
+        .iter()
+        .filter(|wire| wire.name.starts_with("x"))
+        .collect::<Vec<&Wire>>();
+    let mut y_values = wires
+        .iter()
+        .filter(|wire| wire.name.starts_with("y"))
+        .collect::<Vec<&Wire>>();
+
+    let mut z_values = wires
+        .iter()
+        .filter(|wire| wire.name.starts_with("z"))
+        .collect::<Vec<&Wire>>();
+
+    z_values.sort_by(|a, b| a.name.cmp(&b.name));
+    x_values.sort_by(|a, b| a.name.cmp(&b.name));
+    y_values.sort_by(|a, b| a.name.cmp(&b.name));
+
+    let mut adders: Vec<Adder> = Vec::new();
+
+    let carry_0_opt = operations
+        .iter()
+        .filter_map(|op| {
+            if let Operation::And(a, b, c) = op {
+                if a == &x_values[0].name && b == &y_values[0].name {
+                    Some(c)
+                } else if a == &y_values[0].name && b == &x_values[0].name {
+                    Some(c)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<&String>>();
+    let carry_0 = carry_0_opt[0].to_string();
+    let mut prev_carry = carry_0.clone();
+
+    // Create the adders and where an error occurs, it means that the wires should be swapped
+    for i in 1..x_values.len() {
+        let and_input_signals_option = operations.iter()
+            .filter_map(|op| {
+                if let Operation::And(a, b, c) = op {
+                    if a == &x_values[i].name && b == &y_values[i].name {
+                        Some(c)
+                    } else if a == &y_values[i].name && b == &x_values[i].name {
+                        Some(c)
+                    }else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<&String>>();
+        let and_input_signals = and_input_signals_option[0];
+        let xor_input_signals_option = operations.iter()
+            .filter_map(|op| {
+                if let Operation::Xor(a, b, c) = op {
+                    if a == &x_values[i].name && b == &y_values[i].name {
+                        Some(c)
+                    } else if a == &y_values[i].name && b == &x_values[i].name {
+                        Some(c)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<&String>>();
+        let xor_input_signals = xor_input_signals_option[0];
+        println!("xor_input_signals: {:?}", xor_input_signals);
+        let and_before_carry_option = operations.iter()
+            .filter_map(|op| {
+                if let Operation::And(a, b, c) = op {
+                    if a == &xor_input_signals.to_string() && b == &prev_carry.to_string() {
+                        Some(c)
+                    } else if a == &prev_carry.to_string() && b == &xor_input_signals.to_string() {
+                        Some(c)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<&String>>();
+        let and_before_carry = and_before_carry_option[0];
+        let output_option = operations.iter()
+            .filter_map(|op| {
+                if let Operation::Xor(a, b, c) = op {
+                    if a == &prev_carry.to_string() && b == &xor_input_signals.to_string() {
+                        Some(c)
+                    } else if a == &xor_input_signals.to_string() && b == &prev_carry.to_string() {
+                        Some(c)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<&String>>();
+        println!("{:?}", output_option);
+        let output = output_option[0];
+        let output_carry_option = operations.iter()
+            .filter_map(|op| {
+                if let Operation::Or(a, b, c) = op {
+                    if a == &and_before_carry.to_string() && b == &and_input_signals.to_string() {
+                        Some(c)
+                    } else if a == &and_input_signals.to_string() && b == &and_before_carry.to_string() {
+                        Some(c)
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<&String>>();
+        let output_carry = output_carry_option[0];
+        let adder = Adder::new(
+            &x_values[i].name,
+            &y_values[i].name,
+            &prev_carry,
+            and_input_signals,
+            xor_input_signals,
+            and_before_carry,
+            output,
+            output_carry,
+        );
+        adders.push(adder);
+        prev_carry = output_carry.to_string();
+        //println!("Adders: {:?}", adders);
+    }
+    
+    // To check if the swaps work correctly
+    /*let mut x_value: usize = 0;
+    let mut y_value: usize = 0;
+    let mut z_value: usize = 0;
+    for i in 0..x_values.len() {
+        if let Some(value) = x_values[i].value {
+            x_value += (value as usize) << i;
+        }
+        if let Some(value) = y_values[i].value {
+            y_value += (value as usize) << i;
+        }
+    }
+    for i in 0..z_values.len() {
+        if let Some(value) = z_values[i].value {
+            z_value += (value as usize) << i;
+        }
+    }
+
+    let final_value = x_value + y_value;
+    println!("X value: 0{:0b}", x_value);
+    println!("Y value: 0{:0b}", y_value);
+    println!("Z value: {:b}", z_value);
+    println!("F value: {:b}", final_value);
+
+    let mut carries: Vec<u8> = Vec::new();
+    for _ in 0..z_values.len() {
+        carries.push(0);
+    }
+
+    let mut incorrect_wires_names: Vec<String> = Vec::new();
+
+    for i in 0..x_values.len() {
+        if x_values[i].value.unwrap() + y_values[i].value.unwrap() + carries[i] > 1 {
+            carries[i + 1] = 1;
+        }
+        let temp = (x_values[i].value.unwrap() + y_values[i].value.unwrap() + carries[i]) % 2;
+
+        if temp == z_values[i].value.unwrap() {
+            println!("Wire {} is correct", z_values[i].name);
+        } else {
+            println!("Wire {} is incorrect", z_values[i].name);
+            incorrect_wires_names.push(z_values[i].name.clone());
+        }
+    }
+
+    for name in incorrect_wires_names {
+        let related_operations = operations
+            .iter()
+            .filter(|op| {
+                if let Operation::And(_, _, c) = op {
+                    c == &name
+                } else if let Operation::Or(_, _, c) = op {
+                    c == &name
+                } else if let Operation::Xor(_, _, c) = op {
+                    c == &name
+                } else {
+                    false
+                }
+            })
+            .collect::<Vec<&Operation>>();
+
+        println!("Related operations for wire {}: {:?}", name, related_operations);
+    }*/
+}
+
 fn main() {
     let input = include_str!("../input.txt");
     let mut wires: HashSet<Wire> = HashSet::new();
@@ -167,4 +398,7 @@ fn main() {
 
     // PART 1
     println!("Part 1: {}", part1(&wires));
+
+    // PART 2
+    part2(&wires, &operations);
 }
